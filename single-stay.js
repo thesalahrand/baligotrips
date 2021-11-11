a9067ec6f19941c62409c0311f617cf4cf02f3ff
Sourav Malo <sourav.cse5.bu@gmail.com>
2021-11-11 23:04:02 +0600

    6 price inputs added for testing

--- single-stay.js
+++ single-stay.js
@@ -1,11 +1,10 @@
-const prevSiblingSpanIds = ['price-00000056-prefix-acc', 'price-00000058-prefix-acc'];
-// const prevSiblingSpanIds = ['price-00000056-prefix-acc', 'price-00000058-prefix-acc', 'price-0000005a-prefix-acc', 'price-0000005c-prefix-acc', 'price-0000005e-prefix-acc', 'price-00000060-prefix-acc'];
-const hiddenInpIds = ['text-00000068', 'text-0000006a', 'text-0000006c'];
+const prevSiblingSpanIds = ['price-00000056-prefix-acc', 'price-00000058-prefix-acc', 'price-0000005a-prefix-acc', 'price-0000005c-prefix-acc', 'price-0000005e-prefix-acc', 'price-00000060-prefix-acc'];
+const hiddenInpIds = ['price-0000006e-currency-acc', 'price-00000070-currency-acc', 'price-00000072-currency-acc', 'price-00000074-currency-acc', 'price-00000076-currency-acc', 'price-00000078-currency-acc'];
 setInterval(() => {
   prevSiblingSpanIds.forEach((prevSiblingSpanId, idx) => {
-    if(document.querySelector(`#${prevSiblingSpanId}`).nextElementSibling && document.querySelector(`#${hiddenInpIds[idx]}`)) {
+    if(document.querySelector(`#${prevSiblingSpanId}`).nextElementSibling && document.querySelector(`#${hiddenInpIds[idx]}`).nextElementSibling) {
       const actualInp = document.querySelector(`#${prevSiblingSpanId}`).nextElementSibling;
-      const hiddenInp = document.querySelector(`#${hiddenInpIds[idx]}`);
+      const hiddenInp = document.querySelector(`#${hiddenInpIds[idx]}`).nextElementSibling;
       actualInp.value = Math.ceil(actualInp.value);
       hiddenInp.value = actualInp.value;
     }
