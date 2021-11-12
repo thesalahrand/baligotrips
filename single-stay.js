da4f14534171dadf7a09713cb3f154ecd9875b50
Sourav Malo <sourav.cse5.bu@gmail.com>
2021-11-12 10:46:43 +0600

    hiddenInputIds updated

--- single-stay.js
+++ single-stay.js
@@ -1,5 +1,5 @@
 const prevSiblingSpanIds = ['price-00000056-prefix-acc', 'price-00000058-prefix-acc', 'price-0000005a-prefix-acc', 'price-0000005c-prefix-acc', 'price-0000005e-prefix-acc', 'price-00000060-prefix-acc'];
-const hiddenInpIds = ['price-0000006e-currency-acc', 'price-00000070-currency-acc', 'price-00000072-currency-acc', 'price-00000074-currency-acc', 'price-00000076-currency-acc', 'price-00000078-currency-acc'];
+const hiddenInpIds = ['price-00000062-currency-acc', 'price-00000064-currency-acc', 'price-00000066-currency-acc', 'price-00000068-currency-acc', 'price-0000006a-currency-acc', 'price-0000006c-currency-acc'];
 setInterval(() => {
   prevSiblingSpanIds.forEach((prevSiblingSpanId, idx) => {
     if(document.querySelector(`#${prevSiblingSpanId}`).nextElementSibling && document.querySelector(`#${hiddenInpIds[idx]}`).nextElementSibling) {
